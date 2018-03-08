a558006f450068e44e7a69ac04dbb126bf2ddb93
kmezhoud <kmezhoud>
2018-03-08 14:41:51 +0100

    update

--- R/get_kseg_image.R
+++ R/get_kseg_image.R
@@ -3,12 +3,16 @@
 #'
 #' @param file The image that with be segmented as jpg or png file
 #' @param k    The number of classes or labels that with image will be segmented
+#' @param method  default method is kmeans clustering. other available methods 
+#'                pfcm (Possibilistic Fuzzy C-Means Fuzzy),
+#'                fcm (Fuzzy C-Means),
+#'                pam (Partitioning Around Medoids). 
 #'
 #' @return A list of k segmented images and a list of k arrays corresponding to the k segmented images.
 #' @export
 #'
 #' @examples
-get_kseg_image <- function(file, k){
+get_seg_image <- function(file, k, method= 'kmeans'){
   
   im <- EBImage::readImage(file)
   # reshape image into a data frame
@@ -18,20 +22,62 @@ get_kseg_image <- function(file, k){
     blue = matrix(im[,,3], ncol=1)
   )
   
-  ### compute the k-means clustering
+  ### compute the k-means or fcm, or pfcm clustering
   set.seed(1234)
-  K = kmeans(df,k)
-  df$label = K$cluster
+  
+  if(method == 'fcm'){
+    ## reduce the size of image 1/8
+    ims <- resize(im, dim(im)[1]/8)
+    # reshape image into a data frame
+    df <- data.frame(
+      red = matrix(im[,,1], ncol=1),
+      green = matrix(im[,,2], ncol=1),
+      blue = matrix(im[,,3], ncol=1)
+    )
+    
+    cluster.result <- ppclust::fcm(df, centers=k)
+  }else if(method == 'pfcm'){
+    ## reduce the size of image 1/8
+    ims <- resize(im, dim(im)[1]/8)
+    # reshape image into a data frame
+    df <- data.frame(
+      red = matrix(im[,,1], ncol=1),
+      green = matrix(im[,,2], ncol=1),
+      blue = matrix(im[,,3], ncol=1)
+    )
+    cluster.result <- ppclust::pfcm(df, centers=k) 
+  }else if(method == 'pam'){ 
+    ## reduce the size of image 1/8
+    ims <- resize(im, dim(im)[1]/8)
+    # reshape image into a data frame
+    df <- data.frame(
+      red = matrix(ims[,,1], ncol=1),
+      green = matrix(ims[,,2], ncol=1),
+      blue = matrix(ims[,,3], ncol=1)
+    )
+      
+      cluster.result <- cluster::pam(df, k , metric = "euclidean", stand = FALSE)
+      
+      # unify the names of results as il the other clustering methods
+      cluster.result$cluster <- cluster.result$clustering
+      cluster.result$centers <- cluster.result$medoids
+  }else{
+    
+    cluster.result <- stats::kmeans(df,k)
+  }
+  
+  
+  df$label = cluster.result$cluster
   
   ### Replace the color of each pixel in the image with the mean 
   ### R,G, and B values of the cluster in which the pixel resides:
   
   # get the coloring
   colors = data.frame(
-    label = 1:nrow(K$centers), 
-    R = K$centers[,"red"],
-    G = K$centers[,"green"],
-    B = K$centers[,"blue"]
+    label = 1:nrow(cluster.result$centers), 
+    R = cluster.result$centers[,"red"],
+    G = cluster.result$centers[,"green"],
+    B = cluster.result$centers[,"blue"]
   )
   
   # merge color codes on to df
@@ -43,6 +89,7 @@ get_kseg_image <- function(file, k){
   ## define a list of element where we will store segmented matrices
   M <- vector("list", k)
   IM <- vector("list", k)
+  ## loop to plot / store all segmented image
   for(i in seq(k)){
     
     m <- df
@@ -77,8 +124,8 @@ get_kseg_image <- function(file, k){
     M[[paste0("cluster",i, sep="")]] <- EBImage::Image(im.segmented, colormode=Color)
     
     im.segmented <- EBImage::Image(im.segmented, colormode=Color)
-   # plot(im.segmented, title= paste0("Cluster: " ,i, sep= "~"))
-   # text(60, 5, paste0("Cluster: " ,i, sep= "~"), cex = 1.5)
+    # plot(im.segmented, title= paste0("Cluster: " ,i, sep= "~"))
+    # text(60, 5, paste0("Cluster: " ,i, sep= "~"), cex = 1.5)
   }
   return(list(seg_array = IM, seg_image = M))
 }
@@ -101,7 +148,7 @@ merge2img <- function(img1, img2){
   norImg1 <- EBImage::normalize(img1)
   norImg2 <- EBImage::normalize(img2)
   IMG <- EBImage::stackObjects(norImg1, norImg2)
-
+  
   return(IMG)
   
 }
